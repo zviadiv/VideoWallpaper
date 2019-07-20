9cb76f40dc3e315686c68a3fe5a4407a2fe82626
Zviadi V <zviadi@scopicsoftware.com>
2019-07-20 23:02:51 +0400

    Fix qml deployment; Add debug logs

--- VideoWallpaper.pro
+++ VideoWallpaper.pro
@@ -13,6 +13,8 @@ DEFINES += QT_DEPRECATED_WARNINGS QT_DISABLE_DEPRECATED_BEFORE=0x050603
 CONFIG *= c++11
 CONFIG -= app_bundle
 LIBS += -lUser32 -lDwmapi
+CONFIG(release, debug|release): LIBS += -lQt5AV -lQt5AVWidgets
+CONFIG(debug, debug|release): LIBS += -lQt5AVd -lQt5AVWidgetsd
 
 # The following define makes your compiler emit warnings if you use
 # any Qt feature that has been marked deprecated (the exact warnings
@@ -79,7 +81,7 @@ FORMS += \
         $$[QT_INSTALL_BINS]/swscale-*.dll
     isEmpty(windeployqt): windeployqt = $$[QT_INSTALL_BINS]/windeployqt.exe
     exists("$${windeployqt}") {
-        libs.commands = $$quote(\"$${windeployqt}\" --plugindir \"$${BIN_DIR}/plugins\" --force --no-translations --no-system-d3d-compiler --compiler-runtime --no-angle --no-opengl-sw -opengl --no-svg --list source \"$${BIN_DIR}/$${TARGET}.exe\")
+        libs.commands = $$quote(\"$${windeployqt}\" --plugindir \"$${BIN_DIR}/plugins\" --force --no-translations --no-system-d3d-compiler --compiler-runtime --no-angle --no-opengl-sw -opengl --qmldir \"$${ROOT}\" --no-qmltooling --no-quickwidgets --no-svg --list source \"$${BIN_DIR}/$${TARGET}.exe\")
         libs.commands = $$join(libs.commands, $$escape_expand(\\n\\t))
     }
     INSTALLS += libs
--- main.cpp
+++ main.cpp
@@ -1,5 +1,52 @@
 #include "application.h"
 
+#include <QMutex>
+#include <QFile>
+#include <QTextStream>
+
+void fileLogger(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    static QMutex mutex;
+    mutex.lock();
+    QString msgType;
+    switch (type)
+    {
+    case QtDebugMsg:
+        msgType = QStringLiteral("DEBUG");
+        break;
+    case QtInfoMsg:
+        msgType = QStringLiteral("INFORMATION");
+        break;
+    case QtWarningMsg:
+        msgType = QStringLiteral("WARNING");
+        break;
+    case QtCriticalMsg:
+        msgType = QStringLiteral("CRITICAL");
+        break;
+    case QtFatalMsg:
+        msgType = QStringLiteral("FATAL");
+        break;
+    /*case QtSystemMsg:
+        msgType = QStringLiteral("SYSTEM");
+        break;*/
+    default:
+        msgType = QStringLiteral("DEBUG");
+        break;
+    }
+    QString messageStr = QStringLiteral("%0\t%1\t%2\t%3\t%4")
+                .arg(msgType).arg(msg).arg(context.file).arg(context.line).arg(context.function);
+    QString logPath = QCoreApplication::applicationDirPath();
+    logPath += QStringLiteral("/debug.log");
+    QFile file(logPath);
+    if (file.open(QFile::WriteOnly | QFile::Append | QFile::Text))
+    {
+        QTextStream ts(&file);
+        ts << messageStr << QLatin1Char('\n');
+        file.close();
+    }
+    mutex.unlock();
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -7,6 +54,10 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
     QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
     qputenv("QT_ANGLE_PLATFORM", "d3d11");
+
+#ifndef _DEBUG
+    qInstallMessageHandler(fileLogger);
+#endif
 
     Application app(argc, argv);
 
